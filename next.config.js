71c191805807089642cb5ba4c8b56f61d1f2ce0b
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-09-20 18:59:23 +0200

    Fix out folder

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
-  distDir: 'out',
-  basePath: process.env.NODE_ENV === 'production' ? '/ai-prompt-helper' : '',
+  basePath: '',
 }
+
+module.exports = nextConfig;
